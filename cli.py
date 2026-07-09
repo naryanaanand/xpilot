e3002cfce428a747bbb9ab6a08001ec1992835a6
naryanaanand <62480962+naryanaanand@users.noreply.github.com>
2026-07-09 15:46:24 +0530

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,8 +1,14 @@
 import typer
+
 from rich.console import Console
 
-app = typer.Typer(
-    help="XPilot - AI Content Operating System"
-)
+from commands.test import app as test_app
+
+app = typer.Typer()
 
 console = Console()
+
+app.add_typer(
+    test_app,
+    name="test"
+)
